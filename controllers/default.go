0c50899a825514648af36374d097acfcbc0e613c
evan_chou <evan_chou@appdet.com>
2024-10-29 16:51:38 +0800

    refactor: 💡 controller 增加註解

--- controllers/default.go
+++ controllers/default.go
@@ -4,12 +4,18 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 )
 
+// defind a controller
+// we generally defind a controller for a module
 type MainController struct {
+	// 繼承 beego.Controller 的方法與屬性
 	beego.Controller
 }
 
 func (c *MainController) Get() {
+	// Data 是繼承過來的參數, 是 map 類型
 	c.Data["Website"] = "beego.vip"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Email"] = "sherbet912@gmail.com"
+
+	// 選擇要渲染的模板, 框架會在 views 裡面找對應的檔案
 	c.TplName = "index.tpl"
 }
